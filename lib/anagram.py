ef46c0288e6add6d7808ccf15b70a351a51b1010
Gaitano <gaitano.george@student.moringaschool.com>
2023-08-22 08:57:08 +0300

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
 # your code goes here!
+
+List = [
+    "enlists",
+    "google",
+    "inlets",
+    "banana"
+]
+
+class Anagram:
+    def __init__( self, word ):
+        self.word = word
+    
+    def get_word( self ):
+        return self.get_word
+    
+    def set_word( self, word ):
+            self.word = word
+            
+    def match(self, word_list):
+        return [word for word in word_list if sorted(word) == sorted(self.word)]
+        
